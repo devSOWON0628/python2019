ebc919ed8d82cff156de8544d4e20f8b37fba58a
SOWON <48439367+devSOWON0628@users.noreply.github.com>
2022-08-13 00:03:49 +0900

    Delete python0408 directory

--- python0408/venv/10.py
+++
@@ -1,4 +0,0 @@
-year=int(input("연도를 입력하시오"))
-
-if ((year%4==0)and(year%100!=0))or(year%400==0):
-    print("연도가 4로 나누어 떨어지면 윤년이다.")
--- python0408/venv/2.py
+++
@@ -1,4 +0,0 @@
-year=int(input("오늘의 연도를 입력하시오 : "))
-month=int(input("오늘의 월을 입력하시오 : "))
-day=int(input("오늘의 일을 입력하시오 : "))
-print("오늘은",year,"년",month,"월",day,"일 입니다")
